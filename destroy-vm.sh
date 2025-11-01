f621c3f53907b3de07ade5f51cf5efff095dacb7
root <root@pv1.tarams.org>
2025-11-01 07:32:26 +0100

    added new vms

--- destroy-vm.sh
+++ destroy-vm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-qm stop 101 && qm destroy 101 --purge
-qm stop 102 && qm destroy 102 --purge
-qm stop 103 && qm destroy 103 --purge
-qm stop 104 && qm destroy 104 --purge
-qm stop 105 && qm destroy 105 --purge
+qm stop 200 && qm destroy 200 --purge
+qm stop 201 && qm destroy 201 --purge
+qm stop 202 && qm destroy 202 --purge
+qm stop 203 && qm destroy 203 --purge
+# qm stop 105 && qm destroy 105 --purge
